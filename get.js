e3622c49ece9cfc05e2f87c18711d29009d7de71
octonoodle <162030697+octonoodle@users.noreply.github.com>
2024-08-01 17:02:48 -0400

    basic webserver, page stubs

---
+++ get.js
@@ -0,0 +1,59 @@
+const fs = require('fs');
+
+// serve normal html 
+function html(file, response) {
+    fs.readFile('.' + file, function(error, data) {
+        if (error) {
+            error404(file, response);
+        } else {
+            response.writeHead(200, {'Content-Type': 'text/html'});
+            response.write(data);
+            console.log('served url ' + file + ' successfully');
+        }
+        response.end();
+    });
+}
+
+function error404(path, response) {
+    response.writeHead(404, {'Content-Type': 'text/html'});
+    fs.readFile('pages/util/404.html', (error, file404) => {
+        if (error) {
+            response.write("<h1>404</h1>");
+            console.log("failed to fetch 404 page");
+        } else {
+            // file404.getElementById('err-file').innerHTML = path;
+            response.write(file404);
+            console.log('404 error on url ' + path);
+        }
+        response.end();
+    });
+    
+}
+
+function error501(extension, response) {
+    response.writeHead(501, {'Content-Type': 'text/html'});
+    response.write('<h1>501</h1>')
+    response.write('<p>server error: cannot process ' + extension + ' file</p>');
+    response.end();
+}
+
+function error503(response) {
+    response.writeHead(503, {'Content-Type': 'text/html'});
+    fs.readFile('site-down.html', (error,data) => {
+        if (error) {
+            response.write('<h1>503: Server Down</h1>');
+        } else {
+            response.write(data);
+        }
+        response.end();
+    });
+    
+    console.log('ignored url ' + request.url + ' during server maintenance');
+}
+
+module.exports = {
+    html,
+    error501,
+    error503,
+    error404
+};
---
+++ httpserver.js
@@ -0,0 +1,49 @@
+const get = require('./get');
+const http = require('http');
+const fs = require('fs');
+const port = 80;
+const down = false;
+
+const server = http.createServer(function(request, response) {
+    // response.write('hello i am serber');
+    // response.end();
+    
+    let i = request.url.lastIndexOf('.');
+    if (i > -1) { // file directly requested
+        let extension = request.url.substring(i);
+        console.log('url: ' + request.url + ', file type: ' + extension);
+
+        if (down) get.error503(response);
+
+        // serve simple html
+        if (extension === '.html') {
+            get.html(request.url, response);
+        } else {
+            get.error501(extension, response);
+        }
+    } else { // using extensionless name
+        fs.readFile('pages/sources.json', (error, data) => {
+            if (error) {
+                console.log('no such file pages/sources.json');
+                throw new Error('404 on ' + request.url);
+            } else {
+                let json = JSON.parse(data);
+                let sourceKey = Object.keys(json).find(str => str === request.url);
+                let page = json[sourceKey];
+                if (page) {
+                    get.html(page, response)
+                } else {
+                    get.error404(page, response);
+                }
+            }
+        })
+    }
+});
+
+server.listen(port, function(error) {
+    if (error) {
+        console.log('something went wrong, got error' + error);
+    } else {
+        console.log('server listening on port: ' + port);
+    }
+});
