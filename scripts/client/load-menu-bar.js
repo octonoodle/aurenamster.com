06212bfaa2eeb35ba2ea5b416cc3408a5ffb67e5
octonoodle <162030697+octonoodle@users.noreply.github.com>
2025-12-05 12:46:45 -0500

    semi-responsive ringularity

--- scripts/client/load-menu-bar.js
+++ scripts/client/load-menu-bar.js
@@ -1,11 +1,23 @@
 // load menu bar
 window.onload = function() {
+    // wrap content
+    document.body.innerHTML = `<div id="top-level-content">${document.body.innerHTML}</div>`
+
     // insert menu bar
     document.body.innerHTML = 
     `<nav id="menu-bar"></nav>\n` + 
     document.body.innerHTML;
 
-    $("#menu-bar").load("/util/menubar.html style,.menu-bar > ", colorTheMenu);
+    $("#menu-bar").load("/util/menubar.html style,.menu-bar > ", () => {
+        // insert page footer
+        document.body.innerHTML += `<footer id="the-footer"></footer>`;
+
+        // $("#the-footer").load("/util/footer style,.the-footer > ");
+        colorTheMenu();
+
+        // finish loading event listener
+        document.dispatchEvent(new Event("menu-bar-loaded"));
+    });
 }
 
 function colorTheMenu() {
