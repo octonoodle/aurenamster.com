1ac8c9b4a517af241b089eb6d8732b6dc930e027
octonoodle <162030697+octonoodle@users.noreply.github.com>
2026-03-14 22:12:05 -0400

    separate read-only and read-write db queries in the api BUT WORKING

--- scripts/api/database.js
+++ scripts/api/database.js
@@ -10,33 +10,62 @@ const localQuerier = new Pool({
   port: 5432,
 });
 
-const querier = new Pool({
-  user: "auren",
-  host: "localhost",
+const querierReadOnly = new Pool({
+  user: "website_read",
+  host: "/var/run/postgresql",
   database: "rocketry",
-  password: process.env.PASSWORD,
+  port: 7775,
+});
+
+const querierReadWrite = new Pool({
+  user: "website_write",
+  host: "/var/run/postgresql",
+  database: "rocketry",
+  //password: process.env.PASSWORD,
   port: 7775,
 });
 
 // wrapper for raw query function
 // optional response parameter for quick raw json response
-async function query(query, response) {
+async function queryRaw(query, response, queryType) {
+   let theQuerier;
+  switch (queryType) {
+    case "read-only":
+      theQuerier = querierReadOnly;
+      break;
+    case "read-write":
+      theQuerier = querierReadWrite;
+      break;
+    case "local":
+      theQuerier = localQuerier;
+      break;
+    default:
+      throw new Error(`[database]: choose a query type! (given: '${queryType}'`);
+  }
+
   try {
-    let results = await querier.query(query);
+    let results = await theQuerier.query(query);
     if (response) {
-      console.log('[database/json] serving json to response');
+      console.log(`[database/${queryType}/json] serving json to response`);
       get.json(JSON.stringify(results.rows), response);
     } else {
-      console.log('[database/json] returning raw json data');
+      console.log(`[database/${queryType}/json] returning raw json data`);
       return JSON.stringify(results.rows);
     }
   } catch (error) {
-    console.log('[database] invalid query: '+query);
-    console.log('[database] produced error : "' + error + '"');
+    console.log(`[database/${queryType}] invalid query: ${query}`);
+    console.log(`[database/${queryType}] produced error: "${error}"`);
     if (response) {
       get.error404(query, response);
     }
   }
 }
 
-module.exports = { query };
+function queryReadOnly(query, response) {
+  return queryRaw(query, response, "read-only");
+}
+function queryReadWrite(query, response) {
+  return queryRaw(query, response, "read-write");
+}
+
+module.exports = { queryReadOnly, queryReadWrite };
